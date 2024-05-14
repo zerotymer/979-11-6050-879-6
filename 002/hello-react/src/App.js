0a7feb8de8062136f8de724a3baad21d795b182a
zerotymer <hyunjin.shin@outlook.com>
2024-05-14 17:36:19 +0900

    2.4.4 AND 연산자(&&)를 사용한 조건부 렌더링

--- 002/hello-react/src/App.js
+++ 002/hello-react/src/App.js
@@ -1,11 +1,7 @@
 function App() {
-  const name = '리액트';
+  const name = '리웩트';
 
-  return (
-    <div>
-      {name === '리액트' ? (<h1>리액트입니다.</h1>) : (<h2>리액트가 아닙니다.</h2>)}
-    </div>
-  );
+  return <div>{name === '리액트' && (<h1>리액트입니다.</h1>)}</div>;
 }
 
 export default App;
