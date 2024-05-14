de56ab4cf7fe5d5bf10a8c355f7e24d36d03178b
zerotymer <hyunjin.shin@outlook.com>
2024-05-15 01:08:26 +0900

    3.2.3 모듈 내보내기 및 불러오기

--- 003/hello-react/src/App.js
+++ 003/hello-react/src/App.js
@@ -1,15 +1,7 @@
-import './App.css';
+import MyComponent from "./MyComponent";
 
-function App() {
-  const name = '리액트';
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </div>
-  );
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
