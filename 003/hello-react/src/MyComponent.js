554c3eb14a5e2dec52d71edc3f8018d7a9a8eb75
zerotymer <hyunjin.shin@outlook.com>
2024-05-15 20:54:30 +0900

    3.4.1 클래스형 컴포넌트의 state

--- 003/hello-react/src/MyComponent.js
+++ 003/hello-react/src/MyComponent.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { Component } from 'react';
 import PROPTYPES from 'prop-types';
 
-class MyComponent extends React.Component {
+class MyComponent extends Component {
   
   static defaultProps = {
     name: '기본 이름'
