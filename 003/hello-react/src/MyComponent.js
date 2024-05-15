1819c93fc2af8b8048565854042d7ba0d2ada7f9
zerotymer <hyunjin.shin@outlook.com>
2024-05-15 14:32:56 +0900

    3.3.7 클래스형 컴포넌트에서 props 사용하기

--- 003/hello-react/src/MyComponent.js
+++ 003/hello-react/src/MyComponent.js
@@ -1,24 +1,28 @@
+import React from 'react';
 import PROPTYPES from 'prop-types';
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      안녕하세요, 제 이름은 { name }입니다. <br />
-      children 값은 { children }
-      입니다.
-      <br />
-      제가 좋아하는 숫자는 { favoriteNumber }입니다.
-    </div>
-  );
-};
+class MyComponent extends React.Component {
+  
+  static defaultProps = {
+    name: '기본 이름'
+  };
+  static propTypes = {
+    name: PROPTYPES.string,
+    favoriteNumber: PROPTYPES.number.isRequired
+  };
 
-MyComponent.defaultProps = {
-  name: '기본 이름'
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다. <br />
+        children 값은 {children}
+        입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+      </div>
+    );
+  }
 };
-
-MyComponent.propTypes = {
-  name: PROPTYPES.string,
-  favoriteNumber: PROPTYPES.number.isRequired
-}
 
 export default MyComponent;
