f01640383d408f3d1113e0632d272095a7087915
zerotymer <hyunjin.shin@outlook.com>
2024-05-15 23:06:05 +0900

    4.2.3.1 기본 방식

--- 004/hello-react/src/EventPractice.js
+++ 004/hello-react/src/EventPractice.js
@@ -6,18 +6,35 @@ class EventPractice extends React.Component {
     message: ""
   };
 
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  /// region Event Methods
+  handleChange(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({ message: '' });
+  }
+  /// endregion Event Methods
+
+
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
         <input type="text" name="message" placeholder="아무거나 입력해 보세요"
-          value={ this.state.message }
-          onChange={(e) => { this.setState({message: e.target.value}); }} 
+          value={this.state.message}
+          onChange={ this.handleChange }
         />
-        <button onClick={() => {
-          alert(this.state.message);
-          this.setState({ message: ''} );
-        }}>확인</button>
+        <button onClick={ this.handleClick }>확인</button>
       </div>
     );
   }
