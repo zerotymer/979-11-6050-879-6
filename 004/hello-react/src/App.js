246b363fb926a7804f8c75b9573dfa15ca8a9ff6
zerotymer <hyunjin.shin@outlook.com>
2024-05-15 22:41:39 +0900

    4.2.1 컴포넌트 생성 및 불러오기

---
+++ 004/hello-react/src/App.js
@@ -0,0 +1,7 @@
+import EventPractice from './EventPractice';
+
+const App = () => {
+  return <EventPractice />;
+};
+
+export default App;
---
+++ 004/hello-react/src/EventPractice.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class EventPractice extends React.Component {
+
+    render () {
+        return <div><h1>이벤트 연습</h1></div>;
+    }
+}
+
+export default EventPractice;
