0f895b2df67ce687148bd40dc908227174922aad
zerotymer <hyunjin.shin@outlook.com>
2024-05-17 00:17:25 +0900

    4.2.4 input 여러 개 다루기

--- 004/hello-react/src/EventPractice.js
+++ 004/hello-react/src/EventPractice.js
@@ -3,17 +3,18 @@ import React from 'react';
 class EventPractice extends React.Component {
 
   state = {
+    username: "",
     message: ""
   };
 
   /// region Event Methods
   handleChange = (e) => {
-    this.setState({ message: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleClick = () => {
-    alert(this.state.message);
-    this.setState({ message: "" });
+    alert(this.state.username + ": " + this.state.message);
+    this.setState({ username: "", message: "" });
   };
   /// endregion Event Methods
 
@@ -22,6 +23,10 @@ class EventPractice extends React.Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input type="text" name="username" placeholder="사용자명"
+          value={ this.state.username }
+          onChange={ this.handleChange }
+        />
         <input type="text" name="message" placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={ this.handleChange }
