47f7a638f1cb07f089e0309d48911a3f5a9afa32
zerotymer <hyunjin.shin@outlook.com>
2024-05-17 00:28:48 +0900

    4.3 함수 컴포넌트로 구현해 보기

--- 004/hello-react/src/EventPractice.js
+++ 004/hello-react/src/EventPractice.js
@@ -1,46 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends React.Component {
+const EventPractice = () => {
 
-  state = {
-    username: "",
-    message: ""
+  // const [ username, setUsername ] = useState("");
+  // const [ message, setMessage ] = useState("");
+  
+  // const onChangeUsername = (e) => setUsername(e.target.value);
+  // const onChangeMessage = (e) => setMessage(e.target.value);
+  
+  const [ form, setForm ] = useState({username: "", message: ""});
+  const { username, message } = form;
+  const onChangeForm = (e) => setForm({ ...form, [e.target.name]: e.target.value});
+  
+  const onClick = () => {
+    alert(form.username + ": " + form.message);
+    setForm({username: "", message: ""});
   };
 
-  /// region Event Methods
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter')  onClick();
   };
 
-  handleClick = () => {
-    alert(this.state.username + ": " + this.state.message);
-    this.setState({ username: "", message: "" });
-  };
-
-  handleKeyDown = (e) => {
-    if (e.key === 'Enter')  this.handleClick();
-  };
-  /// endregion Event Methods
-
-
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input type="text" name="username" placeholder="사용자명"
-          value={ this.state.username }
-          onChange={ this.handleChange }
-          onKeyDown={ this.handleKeyDown }
-        />
-        <input type="text" name="message" placeholder="아무거나 입력해 보세요"
-          value={this.state.message}
-          onChange={ this.handleChange }
-          onKeyDown={ this.handleKeyDown }
-        />
-        <button onClick={ this.handleClick }>확인</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input type="text" name="username" placeholder="사용자명"
+        value={ username }
+        onChange={ onChangeForm }
+        onKeyDown={ onKeyDown }
+      />
+      <input type="text" name="message" placeholder="아무거나 입력해 보세요"
+        value={ message }
+        onChange={ onChangeForm }
+        onKeyDown={ onKeyDown }
+      />
+      <button onClick={ onClick }>확인</button>
+    </div>
+  );
 }
 
 export default EventPractice;
