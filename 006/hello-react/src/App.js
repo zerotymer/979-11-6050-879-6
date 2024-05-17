35634adc058db4f8d7924abddbe4b3683f6d16d7
zerotymer <hyunjin.shin@outlook.com>
2024-05-18 00:16:52 +0900

    6.2.2 App 컴포넌트에서 예제 컴포넌트 렌더링

--- 006/hello-react/src/App.js
+++ 006/hello-react/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 
 class App extends React.Component {
 
   render() {
-    return (
-      <div>
-        <ScrollBox ref={ (ref) => this.scrollBox = ref }/>;
-        <button onClick={ () => this.scrollBox.scrollToBottom() }>맨 밑으로</button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
