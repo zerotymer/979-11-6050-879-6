a05205c9d0f3d68b080c43e2dfc8c343a013f91f
zerotymer <hyunjin.shin@outlook.com>
2024-05-17 17:34:51 +0900

    5.1.2 App 컴포넌트에서 예제 컴포넌트 렌더링

--- 004/hello-react/src/App.js
+++ 004/hello-react/src/App.js
@@ -1,7 +1,7 @@
-import EventPractice from './EventPractice';
+import ValidationSample from "./ValidationSample";
 
 const App = () => {
-  return <EventPractice />;
+  return <ValidationSample />;
 };
 
 export default App;
