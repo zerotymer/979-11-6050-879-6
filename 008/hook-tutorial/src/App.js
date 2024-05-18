a43659a7b4f2a82cebde7dcce8edf25a198dfe3f
zerotymer <hyunjin.shin@outlook.com>
2024-05-19 00:16:12 +0900

    8.3.1 카운터 구현하기

--- 008/hook-tutorial/src/App.js
+++ 008/hook-tutorial/src/App.js
@@ -1,16 +1,7 @@
-import { useState } from 'react';
-import Info from './Info';
+import Counter from './Counter';
 
 const App = () => {
-  const [ visible, setVisible ] = useState(false);
-
-  return (
-    <div>
-      <button onClick={ () => setVisible(!visible) }>{ visible ? '숨기기' : '보이기' }</button>
-      <hr />
-      { visible && <Info /> }
-    </div>
-  );
+  return <Counter />;
 };
 
 export default App;
