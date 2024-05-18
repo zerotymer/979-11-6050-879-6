482f7aafaeeb9a4670387b278e02f9368de2df56
zerotymer <hyunjin.shin@outlook.com>
2024-05-19 00:04:05 +0900

    8.2.2 특정 값이 업데이트될 때만 실행하고 싶을 때

--- 008/hook-tutorial/src/Info.js
+++ 008/hook-tutorial/src/Info.js
@@ -5,9 +5,8 @@ const Info = () => {
     const [ nickname, setNickname ] = useState('');
 
     useEffect(() => {
-        console.log('렌더링이 완료되었습니다.');
-        console.log({ name, nickname });
-    });
+        console.log(name);
+    }, [name]);
 
     const onChangeName = e => setName(e.target.value);
     const onChangeNickname = e => setNickname(e.target.value);
