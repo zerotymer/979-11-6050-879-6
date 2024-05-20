fae8bcfd5dde70171ad18337d2d98eec2a7b92a9
zerotymer <hyunjin.shin@outlook.com>
2024-05-20 23:42:08 +0900

    8.6 useRef

--- 008/hook-tutorial/src/Average.js
+++ 008/hook-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중..');
@@ -11,19 +11,21 @@ const getAverage = numbers => {
 const Average = () => {
     const [ list, stateList ] = useState([]);
     const [ number, stateNumber ] = useState('');
+    const inputEl = useRef(null);
 
     const onChange = useCallback(e => { stateNumber(e.target.value); }, []); // 변경없음. 초기 렌더링시만 수행
     const onInsert = useCallback(e => {
         const nextList = list.concat(parseInt(number)); // concat() returns a new array
         stateList(nextList);
         stateNumber('');
+        inputEl.current.focus();
     }, [number, list]); // 초기 렌더링, number, list가 변경시에 리랜더링
 
     const avg = useMemo(() => getAverage(list), [list]);
 
     return (
         <div>
-            <input value={ number } onChange={ onChange } />
+            <input value={ number } onChange={ onChange } ref={ inputEl }/>
             <button onClick={ onInsert }>등록</button>
             <ul>
                 { list.map((value, index) => <li key={ index }>{ value }</li>) }
