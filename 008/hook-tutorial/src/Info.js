f51cdf326e01d6f3c6a4c91cd8ec7828a5100128
zerotymer <hyunjin.shin@outlook.com>
2024-05-20 23:54:15 +0900

    8.7 커스텀 Hooks 만들기

--- 008/hook-tutorial/src/Info.js
+++ 008/hook-tutorial/src/Info.js
@@ -1,20 +1,13 @@
-import { useReducer  } from 'react';
-
-function reducer(state, action) {
-    return {
-        ...state,
-        [action.name]: action.value     // input element name, input element value
-    };
-}
+import useInputs from './useInputs';
 
 const Info = () => {
-    const [ state, dispatch ] = useReducer(reducer, {
+    const [ state, onChange ] = useInputs({
         name: '',
         nickname: ''
     });
     const { name, nickname } = state;
 
-    const onChange = e => { dispatch(e.target); };
+    // const onChange = e => { dispatch(e.target); };
 
     return (
         <div>
