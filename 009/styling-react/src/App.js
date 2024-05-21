c3f579f0eb6adf1b35e326345efd3735cc69dc8a
zerotymer <hyunjin.shin@outlook.com>
2024-05-22 01:18:54 +0900

    9.1.2 CSS Selector

--- 009/styling-react/src/App.js
+++ 009/styling-react/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <img src={logo} className="logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
