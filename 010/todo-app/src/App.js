8dd0c786e084ade3e8e8d24382df693ce795e4d9
zerotymer <hyunjin.shin@outlook.com>
2024-05-23 14:16:05 +0900

    10.2.2 TodoInsert 만들기

--- 010/todo-app/src/App.js
+++ 010/todo-app/src/App.js
@@ -1,5 +1,6 @@
+import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 
-const App = () => <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+const App = () => <TodoTemplate><TodoInsert /></TodoTemplate>;
 
 export default App;
