fad162cd869b107b4eb061534b51af1a8c04f0d8
zerotymer <hyunjin.shin@outlook.com>
2024-05-23 14:49:05 +0900

    10.3.2 항목 추가 기능 구현하기

--- 010/todo-app/src/App.js
+++ 010/todo-app/src/App.js
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
 const App = () => {
-    
+
     const [ todos, setTodos] = useState([
         { id: 1, text: '리액트의 기초 알아보기', checked: true },
         { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
         { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
     ]);
 
+    // 고유값으로 사용될 id
+    // ref를 사용하여 변수 담기
+    const nextId = useRef(4);
+
+    const onInsert = useCallback((text) => {
+        const todo = { id: nextId.current, text, checked: false };
+        setTodos(todos.concat(todo));   // 기존과 병합하여 새로 생성
+        nextId.current += 1;
+    }, [todos]);
+
     return (
         <TodoTemplate>
-            <TodoInsert />
+            <TodoInsert onInsert={ onInsert }/>
             <TodoList todos={ todos } />
         </TodoTemplate>
     );
--- 010/todo-app/src/components/TodoInsert.js
+++ 010/todo-app/src/components/TodoInsert.js
@@ -1,10 +1,25 @@
+import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [ value, setValue ] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []); // 리렌더링 없음.
+    
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue(''); // value 초기화
+
+        // submit 이벤트의 브라우저 새로고침을 방지.
+        e.preventDefault();
+    }, [ onInsert, value]);
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요" />
+        <form className="TodoInsert" onSubmit={ onSubmit }>
+            <input placeholder="할 일을 입력하세요" value={ value } onChange={ onChange }/>
             <button type="submit">
                 <MdAdd />
             </button>
