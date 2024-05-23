ca136619eefe6a37ae908173f8c7f807b71b5bee
zerotymer <hyunjin.shin@outlook.com>
2024-05-23 18:02:16 +0900

    11.8.2 TodoList 수정

--- 010/todo-app/src/components/TodoList.js
+++ 010/todo-app/src/components/TodoList.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import './TodoList.scss';
 import TodoListItem from "./TodoListItem";
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+
+    const rowRenderer = useCallback(row => {
+        const { index, key, style } = row;
+        const todo = todos[index];
+        return (
+            <TodoListItem todo={ todo } key={ todo.id } onRemove={ onRemove } onToggle={ onToggle } style={ style }/>
+        );
+    }, [ todos, onRemove, onToggle ]);
+
     return (
-        <div className="TodoList">
-            { todos.map(todo => (
-                <TodoListItem todo={ todo } key={ todo.id } onRemove={ onRemove } onToggle={ onToggle }/>
-            ))}
-        </div>
+        <List className="TodoList" style={{ outline: 'none' }}
+            width={ 512 } height={ 513 }
+            rowCount={ todos.length } rowHeight={ 57 } rowRenderer={ rowRenderer } 
+            list={ todos } />
     );
 };
 
