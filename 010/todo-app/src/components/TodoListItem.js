1b58a0ec10fac209aa9ff2c3fb36ed13861e6eef
zerotymer <hyunjin.shin@outlook.com>
2024-05-23 15:25:13 +0900

    10.3.4 수정 기능

--- 010/todo-app/src/components/TodoListItem.js
+++ 010/todo-app/src/components/TodoListItem.js
@@ -2,12 +2,12 @@ import cn from 'classnames';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
     const { id, text, checked } = todo;
 
     return (
         <div className="TodoListItem">
-            <div className={cn('checkbox', { checked })} >
+            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
                 { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 <div className="text">{ text }</div>
             </div>
