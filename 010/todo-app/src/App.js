48bb40530163718d7f94c80917a09d31c8af0a49
zerotymer <hyunjin.shin@outlook.com>
2024-05-23 14:17:33 +0900

    10.2.3 TodoListItem과 TodoList 만들기

--- 010/todo-app/src/App.js
+++ 010/todo-app/src/App.js
@@ -1,6 +1,12 @@
-import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
-const App = () => <TodoTemplate><TodoInsert /></TodoTemplate>;
+const App = () => (
+    <TodoTemplate>
+        <TodoInsert />
+        <TodoList />
+    </TodoTemplate>
+);
 
 export default App;
