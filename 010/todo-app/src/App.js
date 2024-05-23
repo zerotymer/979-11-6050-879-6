eb843e61a09eb2b8f7a310a64f55c62e2986d217
zerotymer <hyunjin.shin@outlook.com>
2024-05-23 17:26:05 +0900

    11.5.2 useReducer 사용하기

--- 010/todo-app/src/App.js
+++ 010/todo-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useCallback } from "react";
+import { useReducer, useRef, useCallback } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
@@ -15,9 +15,25 @@ function createBulkTodos() {
     return array;
 }
 
+function todoReducer(todos, action) {
+    switch (action.type) {
+        case 'INSERT': // 새로 추가
+            // { type: 'INSERT', todo: { id: 1, text: 'todo', checked: false } }
+            return todos.concat(action.todo);
+        case 'REMOVE': // 제거
+            // { type: 'REMOVE', id: 1 }
+            return todos.filter(todo => todo.id !== action.id);
+        case 'TOGGLE': // 토글
+            // { type: 'TOGGLE', id: 1 }
+            return todos.map(todo => todo.id === action.id ? { ...todo, checked: !todo.checked } : todo);
+        default:
+            return todos;
+    }
+}
+
 const App = () => {
 
-    const [ todos, setTodos ] = useState(createBulkTodos);
+    const [ todos, dispatch ] = useReducer(todoReducer, undefined, createBulkTodos);
 
     // 고유값으로 사용될 id
     // ref를 사용하여 변수 담기
@@ -25,18 +41,16 @@ const App = () => {
 
     const onInsert = useCallback((text) => {
         const todo = { id: nextId.current, text, checked: false };
-        setTodos((todos) => todos.concat(todo));   // 기존과 병합하여 새로 생성
+        dispatch({ type: 'INSERT', todo })
         nextId.current += 1;
     }, []);
 
     const onRemove = useCallback((id) => {
-        setTodos((todos) => todos.filter(todo => todo.id !== id));
+        dispatch({ type: 'REMOVE', id })
     }, []);
 
     const onToggle = useCallback((id) => {
-        setTodos((todos) => todos.map(todo => 
-            todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        ));
+        dispatch({ type: 'TOGGLE', id })
     }, []);
 
     return (
