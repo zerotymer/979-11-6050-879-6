a40e978e3e14975282243653ed2b80b7344996ec
zerotymer <hyunjin.shin@outlook.com>
2024-05-25 12:22:36 +0900

    13.4.1 URL 파라미터

--- 013/router-tutorial/src/App.js
+++ 013/router-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -8,11 +9,15 @@ function App() {
       <ul>
         <li><a href="/">홈</a></li>
         <li><a href="/about">소개</a></li>
+        <li><a href="/profile/velopert">velopert 프로필</a></li>
+        <li><a href="/profile/gildong">gildong 프로필</a></li>
       </ul>
       <hr />
       <Routes>
-        <Route path="/" exact element={ <Home /> } />
-        <Route path={['/about', '/info']} element={ <About /> } />
+        <Route path="/"                   element={ <Home /> } />
+        <Route path="/about"              element={ <About /> } />
+        <Route path="/info"               element={ <About /> } />
+        <Route path="/profile/:username"  element={ <Profile /> } />
       </Routes>
     </div>
   );
