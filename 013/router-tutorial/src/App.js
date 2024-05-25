422461b70e313259c495d067e91b99a17f1f0df4
zerotymer <hyunjin.shin@outlook.com>
2024-05-25 12:12:24 +0900

    13.2.5 Link 컴포넌트를 사용하여 다른 주소로 이동하기

--- 013/router-tutorial/src/App.js
+++ 013/router-tutorial/src/App.js
@@ -5,6 +5,11 @@ import About from './About';
 function App() {
   return (
     <div>
+      <ul>
+        <li><a href="/">홈</a></li>
+        <li><a href="/about">소개</a></li>
+      </ul>
+      <hr />
       <Routes>
         <Route path="/" exact element={ <Home /> } />
         <Route path="/about" element={ <About /> } />
