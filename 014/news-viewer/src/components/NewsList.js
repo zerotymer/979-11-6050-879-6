6fd4e1f782c6d3a4cbaca4246e30f0436aad1117
zerotymer <hyunjin.shin@outlook.com>
2024-05-28 15:32:28 +0900

    14.5 데이터 연동하기

--- 014/news-viewer/src/components/NewsList.js
+++ 014/news-viewer/src/components/NewsList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
@@ -22,15 +23,50 @@ const sampleArticle = {
     urlToImage: 'https://via.placeholder.com/160',
 };
 
+const API_KEY = '0c7954c1ed8d4b1cb9db421301ae381b';
+const getUrl = (country = 'kr', apikey = API_KEY, category) => {
+  let url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apikey}`;
+  if (category) {
+    url += `&category=${category}`;
+  }
+  return url;
+};
+
 const NewsList = () => {
+    const [ articles, setArticles ] = useState(null);
+    const [ loading, setLoading ] = useState(false);
+
+    useEffect(() => {
+        // async를 사용하는 함수 따로 선언
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(getUrl(), );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+
+        fetchData();
+    }, []);
+
+    // 대기 중일 때
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+
+    // articles 값이 유효할 때
     return (
         <NewsListBlock>
-            <NewsItem article={ sampleArticle } />
-            <NewsItem article={ sampleArticle } />
-            <NewsItem article={ sampleArticle } />
-            <NewsItem article={ sampleArticle } />
-            <NewsItem article={ sampleArticle } />
-            <NewsItem article={ sampleArticle } />
+            { articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     );
 };
