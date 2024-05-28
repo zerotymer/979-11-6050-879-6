e91e0f9487d269f55683dd24c31aa035dafe6943
zerotymer <hyunjin.shin@outlook.com>
2024-05-28 18:12:52 +0900

    14.7.2 NewsPages 생성

--- 014/news-viewer/src/App.js
+++ 014/news-viewer/src/App.js
@@ -1,16 +1,12 @@
 import React, { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Routes, Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [ category, setCategory ] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
   return (
-    <>
-      <Categories category={ category } onSelect={ onSelect }/>
-      <NewsList category={ category }/>
-    </>
+    <Routes>
+      <Route path="/:category?" element={<NewsPage />} />
+    </Routes>
   );
 };
 
