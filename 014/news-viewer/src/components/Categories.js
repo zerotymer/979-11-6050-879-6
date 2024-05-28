0de259a599ddcedb4129d0d2ee1b111f3944c411
zerotymer <hyunjin.shin@outlook.com>
2024-05-28 18:26:14 +0900

    14.7.3 Categories에서 NaviLink 사용하기

--- 014/news-viewer/src/components/Categories.js
+++ 014/news-viewer/src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
     { name: 'all', text: '전체보기' },
@@ -24,7 +25,7 @@ const CategoriesBlock = styled.div`
     }
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
     font-size: 1.125rem;
     cursor: pointer;
     white-space: pre;
@@ -34,6 +35,15 @@ const Category = styled.div`
 
     &:hover {
         color: #495057;
+    }
+
+    &.active {
+        font-weight: 600;
+        border-bottom: 2px solid #22b8cf;
+        color: #22b8cf;
+        &:hover {
+            color: #3bc9db;
+        }
     }
 
     ${ props => props.active && css`
@@ -54,9 +64,11 @@ const Categories = ({ onSelect, category }) => {
     return (
         <CategoriesBlock>
             {categories.map(c => (
-                <Category key={c.name} 
-                    active={ category === c.name }
-                    onClick={ () => onSelect(c.name) } 
+                <Category 
+                    key={c.name} 
+                    activeClassName="active"
+                    exact={c.name === 'all'}
+                    to={c.name === 'all' ? '/' : `/${c.name}`}
                 >
                     {c.text}
                 </Category>
