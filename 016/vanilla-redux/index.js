b8de01ef73c0ce7ee0b01e33c11f26295de24d75
zerotymer <hyunjin.shin@outlook.com>
2024-05-31 01:01:12 +0900

    16.2.6 리듀서 함수 정의

--- 016/vanilla-redux/index.js
+++ 016/vanilla-redux/index.js
@@ -3,15 +3,28 @@ const counter = document.querySelector('h1');
 const increment = document.querySelector('#increment');
 const decrement = document.querySelector('#decrement');
 
+/// Action Types
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 
+/// Action Creators
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const incrementCounter = () => ({ type: INCREMENT });
 const decrementCounter = () => ({ type: DECREMENT });
 
+/// initialState
 const initialState = {
     toggle: false,
     counter: 0
 };
+
+/// Reducer
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case TOGGLE_SWITCH:     return { ...state, toggle: !state.toggle};
+        case INCREMENT:         return { ...state, counter: state.counter + 1 };
+        case DECREMENT:         return { ...state, counter: state.counter - 1 };
+        default:                return state;
+    }
+}
