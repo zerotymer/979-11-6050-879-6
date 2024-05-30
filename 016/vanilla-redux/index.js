c52af1e36912314534fc9b2144fbcde03926a10d
zerotymer <hyunjin.shin@outlook.com>
2024-05-31 00:54:04 +0900

    16.2.4 액션 타입과 액션 생성 함수 정의

--- 016/vanilla-redux/index.js
+++ 016/vanilla-redux/index.js
@@ -2,3 +2,11 @@ const divToogle = document.querySelector('.toogle');
 const counter = document.querySelector('h1');
 const increment = document.querySelector('#increment');
 const decrement = document.querySelector('#decrement');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const incrementCounter = () => ({ type: INCREMENT });
+const decrementCounter = () => ({ type: DECREMENT });
