707841a0c70c9c03964186f61047da87165402e2
zerotymer <hyunjin.shin@outlook.com>
2024-06-01 02:04:14 +0900

    17.5.2 TodosContainer 만들기

--- 017/react-redux-tutorial/src/containers/CounterContainer.js
+++ 017/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,3 @@
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
