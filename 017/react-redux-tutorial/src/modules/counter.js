ab68fddfa5d6a3920fb4d88ae57c0b92dc445748
zerotymer <hyunjin.shin@outlook.com>
2024-05-31 23:49:30 +0900

    17.3.1 Counter 모듈 작성하기

---
+++ 017/react-redux-tutorial/src/modules/counter.js
@@ -0,0 +1,27 @@
+/// region Actions
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+/// endregion
+
+
+/// region Action Creators
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+/// endregion
+
+
+/// region Reducer
+const initialState = {
+  number: 0
+};
+
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:  return { number: state.number + 1 };
+    case DECREASE:  return { number: state.number - 1 };
+    default:        return state;
+  }
+}
+/// endregion
+
+export default counter;
