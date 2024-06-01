984be59b2a22caa04a4d471f4977fa190617a1df
zerotymer <hyunjin.shin@outlook.com>
2024-06-01 18:52:25 +0900

    17.6.2 immer
    yarn add immer

--- 017/react-redux-tutorial/src/modules/todos.js
+++ 017/react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
 
 /// region Actions
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
@@ -28,16 +29,20 @@ const initialState = {
   ]
 };
 const todos = handleActions({
-  [CHANGE_INPUT]: (state, { payload : input}) => ({ ...state, input }),
-  [INSERT]: (state, { payload: todo}) => ({ 
-    ...state, 
-    todos: state.todos.concat(todo) }),
-  [TOGGLE]: (state, { payload: id }) => ({ 
-    ...state, 
-    todos: state.todos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo ) }),
-  [REMOVE]: (state, { payload: id }) => ({ 
-    ...state,
-    todos: state.todos.filter(todo => todo.id !== id )})
+  [CHANGE_INPUT]: (state, { payload : input}) => produce(state, draft => { 
+    draft.input = input 
+  }),
+  [INSERT]: (state, { payload: todo}) => produce(state, draft => { 
+    draft.todos.push(todo)
+  }),
+  [TOGGLE]: (state, { payload: id }) => produce(state, draft => { 
+    const todo = draft.todos.find(todo => todo.id === id);
+    todo.done = !todo.done;
+  }),
+  [REMOVE]: (state, { payload: id }) => produce(state, draft => {
+    const index = draft.todos.findIndex(todo => todo.id === id);
+    draft.todos.splice(index, 1);
+  })
 }, initialState);
 /// endregion
 
