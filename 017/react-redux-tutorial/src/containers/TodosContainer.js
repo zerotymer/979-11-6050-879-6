ee538274059ee927340dbf7ea1fa1009501232d5
zerotymer <hyunjin.shin@outlook.com>
2024-06-02 22:33:38 +0900

    17.7.5 useActions 유틸 Hook을 만들어서 사용하기

--- 017/react-redux-tutorial/src/containers/TodosContainer.js
+++ 017/react-redux-tutorial/src/containers/TodosContainer.js
@@ -2,17 +2,15 @@ import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
     const { input, todos } = useSelector(({ todos }) => ({
         input: todos.input,
         todos: todos.todos
     }));
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
-    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
-    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
-    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+    const [ onChangeInput, onInsert, onToggle, onRemove ] = useActions([changeInput, insert, toggle, remove], []);
 
     return (
         <Todos 
