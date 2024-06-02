04ae89f2ad91ee05024fe76feaa211f0cbd46d64
zerotymer <hyunjin.shin@outlook.com>
2024-06-02 21:39:49 +0900

    17.7.1 useSelector로 상태 조회하기

--- 017/react-redux-tutorial/src/containers/CounterContainer.js
+++ 017/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,22 +1,9 @@
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-/// region Redux Mapper
-// store의 state(store.getState())를 받아와 props로 컨테이너에 전달
-// const mapStateToProps = (state) => ({
-//     number: state.counter.number
-// });
-// dispatch함수(store.dispatch(action))을 정의하여 props로 컨테이너에 전달
-// 여기선 dispatch(action creator)를 정의
-// const mapDispatchtoProps = (dispatch) => ({
-//     increase: () => dispatch(increase()),
-//     decrease: () => dispatch(decrease())
-// });
-/// endregion
-
-
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = ({ increase, decrease }) => {
+    const number = useSelector(state => state.counter.number);
     return (
         <Counter 
             number={ number }
