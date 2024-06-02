8a7395ace0a4f116c450129f02b81ecbcee7cf9b
zerotymer <hyunjin.shin@outlook.com>
2024-06-02 21:51:30 +0900

    17.7.4 TodosContainer를 Hooks로 전환하기

--- 017/react-redux-tutorial/src/containers/TodosContainer.js
+++ 017/react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,21 +1,29 @@
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 
-const TodosContainer = ({ input, todos, changeInput, insert, toggle, remove }) => {
+const TodosContainer = () => {
+    const { input, todos } = useSelector(({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos
+    }));
+    const dispatch = useDispatch();
+    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
     return (
         <Todos 
             input={ input }
             todos={ todos }
-            onChangeInput={ changeInput }
-            onInsert={ insert }
-            onToggle={ toggle }
-            onRemove={ remove }
+            onChangeInput={ onChangeInput }
+            onInsert={ onInsert }
+            onToggle={ onToggle }
+            onRemove={ onRemove }
         />
     );
 };
 
-export default connect(
-    ({ todos }) => ({ input: todos.input, todos: todos.todos }),
-    { changeInput, insert, toggle, remove }
-)(TodosContainer);
+export default TodosContainer;
