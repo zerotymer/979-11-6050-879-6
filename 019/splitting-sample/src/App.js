88348024ee36d79a84e5be1d3e3171aaa92bab4d
zerotymer <hyunjin.shin@outlook.com>
2024-06-07 00:52:01 +0900

    19.1 자바스크립트 함수 비동기 로딩

--- 019/splitting-sample/src/App.js
+++ 019/splitting-sample/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+// import notify from './notify';
 
 function App() {
+
+  const onClick = () => {
+    // notify();
+    import('./notify').then(notify => notify.default());
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
+        <p onClick={ onClick }>Hello React!</p>
       </header>
     </div>
   );
---
+++ 019/splitting-sample/src/notify.js
@@ -0,0 +1,3 @@
+export default function notify() {
+    console.log('안녕하세요!');
+}
